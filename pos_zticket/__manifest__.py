14c16a5a61166777f3fe6251a2293e2e21c6548f
antoinevrac <84444789+antoinevrac@users.noreply.github.com>
2021-06-16 16:16:52 +0200

    last last pos

---
+++ pos_zticket/__manifest__.py
@@ -0,0 +1,13 @@
+# Copyright 2019 Coop IT Easy SCRLfs
+#           Pierrick Brun <pierrick.brun@akretion.com>
+{
+    "name": "PoS Z Ticket",
+    "version": "12.0.1.0.0",
+    "summary": """Download a Z ticket as .xls""",
+    "author": "Coop IT Easy SCRLfs, ",
+    "website": "https://vracoop.fr/",
+    "category": "Point of Sale",
+    "depends": ["point_of_sale", "report_xlsx_helper",],
+    "data": ["views/pos_views.xml"],
+    "installable": True,
+}
