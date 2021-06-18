dac7690a5201814f9df1fe5acfc574c724c5e080
antoinevrac <84444789+antoinevrac@users.noreply.github.com>
2021-06-18 16:36:44 +0200

    add pos ticket log without init

--- __manifest__.py
+++ __manifest__.py
@@ -1,29 +1,21 @@
-# Copyright 2020 Coop IT Easy SCRL fs
-#   Vincent Van Rossem <vincent@coopiteasy.be>
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
 {
-    "name": "Vracoop Custom Menu",
+    "name": "Pos Ticket Logo",
+    "category": "Point Of Sale",
     "version": "12.0.1.0.0",
-    "summary": """Menus""",
-    "author": "Coop IT Easy SCRLfs, ",
-    "website": "https://vracoop.fr/",
-    "category": "Menu Items",
+    "author": "Tecnativa, "
+              "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/pos",
+    "license": "AGPL-3",
     "depends": [
-        "account",
         "point_of_sale",
-        "pos_container",
-        "purchase",
-        "sale",
-        "stock",
     ],
     "data": [
-        "views/account_menuitem.xml",
-        "views/container.xml",
-        "views/point_of_sale_view.xml",
-        "views/pos_config_view.xml",
-        "views/purchase_views.xml",
-        "views/sale_views.xml",
-        "views/stock_menu_views.xml",
+        "templates/assets.xml",
+    ],
+    "qweb": [
+        "static/src/xml/pos.xml",
     ],
     "installable": True,
 }
